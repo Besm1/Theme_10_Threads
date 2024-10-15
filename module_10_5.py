c403a180d491b132d1a6df66a2d2b3932ad15a14
b.smirnov <besm1@yandex.ru>
2024-10-15 18:53:38 +0300

    module_10_5

--- module_10_5.py
+++ module_10_5.py
@@ -10,18 +10,31 @@ def read_info(name):
             all_data.append(line)
             line = file.readline()
 
+def time_dec(proc):
+    def kostyl(*args, **kwargs):
+        start = datetime.datetime.now()
+        result = proc(*args, **kwargs)
+        time = (datetime.datetime.now() - start)
+        print(time)
+        return result
+    return kostyl
+
+@time_dec
+def classic(*files):
+    for f_ in files[0]:
+        read_info(f_)
+
+@time_dec
+def multy(*files):
+    with multiprocessing.Pool(processes=4) as pool:
+        pool.map(read_info, files[0] )
+
+@time_dec
+def main():
+    files = [f'./files_10_5/file {i}.txt' for i in range(1,5)]
+    classic(files)
+    multy(files)
 
 if __name__ == '__main__':
-    files = [f'./files_10_5/file {i}.txt' for i in range(1,5)]
+    main()
 
-    start = datetime.datetime.now()
-    for f_ in files:
-        read_info(f_)
-    finish = datetime.datetime.now()
-    print(finish - start)
-
-    with multiprocessing.Pool(processes=4) as pool:
-        start = datetime.datetime.now()
-        pool.map(read_info, files )
-    finish = datetime.datetime.now()
-    print(finish - start)
